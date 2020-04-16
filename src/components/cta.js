a19b777476bc6b2df492739394b1d473d58c9b0e
Carlo Schiesaro <carloschiesaro@carlos-imac.mynet>
2020-04-16 16:19:43 +0100

    fixed form

--- src/components/cta.js
+++ src/components/cta.js
@@ -14,7 +14,9 @@ export default () => {
         <button className="btn" onClick={handleClick}>Hire Me!</button>
       </div>
       {formVisible &&
-        <form className="form" name="contact" method="POST" data-netlify="true" action="/thankyou">
+        <form action="/thankyou" data-netlify="true" netlify-honeypot="bot-field" method="POST" name="contact" className="form">
+          <input type="hidden" name="form-name" value="contact" />
+          <input type="hidden" name="bot-field" />
           <input name="name" type="text" placeholder="Your name and/or company name" required />
           <input name="email" type="email" placeholder="Your e-mail" required />
           <input name="phone" type="tel" placeholder="Your phone number" required />
