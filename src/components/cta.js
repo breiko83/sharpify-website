53d9a7fbf5796a24a14279bae55d07af0fbf9a23
Carlo Schiesaro <carloschiesaro@carlos-imac.mynet>
2020-04-16 17:11:09 +0100

    Bugfix

--- src/components/cta.js
+++ src/components/cta.js
@@ -13,17 +13,15 @@ export default () => {
       <div className="cta-landing">
         <button className="btn" onClick={handleClick}>Hire Me!</button>
       </div>
-      {formVisible &&
-        <form action="/thankyou" data-netlify="true" netlify-honeypot="bot-field" method="POST" name="contact" className={formVisible ? 'form' : 'form hidden'}>
-          <input type="hidden" name="form-name" value="contact" />
-          <input type="hidden" name="bot-field" />
-          <input name="name" type="text" placeholder="Your name and/or company name" required />
-          <input name="email" type="email" placeholder="Your e-mail" required />
-          <input name="phone" type="tel" placeholder="Your phone number" required />
-          <textarea name="request" placeholder="Add some infos"></textarea>
-          <input type="submit" />
-        </form>
-      }
+      <form action="/thankyou" data-netlify="true" netlify-honeypot="bot-field" method="POST" name="contact" className={formVisible ? 'form' : 'form hidden'}>
+        <input type="hidden" name="form-name" value="contact" />
+        <input type="hidden" name="bot-field" />
+        <input name="name" type="text" placeholder="Your name and/or company name" required />
+        <input name="email" type="email" placeholder="Your e-mail" required />
+        <input name="phone" type="tel" placeholder="Your phone number" required />
+        <textarea name="request" placeholder="Add some infos"></textarea>
+        <input type="submit" />
+      </form>
     </div>
   )
 }
